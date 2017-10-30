94a3eacfb332eb90377d4527ef73e4d45cc4d23c
Awtian Akbar <me@awtian.com>
2017-10-30 09:34:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function theBeatlesPlay(name, inst) {
   var b = []
-  for (var i = 1; i < 100; i++) {
+  for (var i = 1; i < name.length; i++) {
     b.push name[i+1] +" plays "+ inst[i+1]
     return b
   }
