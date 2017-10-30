bc796146344903f3ba5c5735afc2ca26665f42ac
Awtian Akbar <me@awtian.com>
2017-10-30 09:25:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ function theBeatlesPlay(name, inst) {
   var b = []
   for (i = 0, i < name.length - 1, i++) {
     b.push name[i] +" plays "+ inst[i]
+    return b
   }
 }
 
 function johnLennonFacts(x) {
-  return "x" 
+  return "x"
 }
